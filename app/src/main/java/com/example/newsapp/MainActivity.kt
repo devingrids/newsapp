5c88affc156f682875fc65ffa6546beaabf72465
Ingrid Silva <sinngtec@gmail.com>
2024-03-05 01:20:58 -0300

    implements onboarding flow

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newsapp.onboarding.OnBoardingScreen
 import com.example.newsapp.ui.theme.NewsappTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +17,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsappTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
