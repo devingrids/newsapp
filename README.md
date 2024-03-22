b2a446cf716d0aa0f628b026bb3da1f2d4ed993a
Ingrid Silva <sinngtec@gmail.com>
2024-03-22 01:58:27 -0300

    git add new constants

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # newsapp
+
+An Android App built with Jetpack Compose.
+
+Things I used to build this App:
+- Jetpack Compose
+- Clean Architecture
+- MVVM & MVI
+- Pagging 3
+- Retrofit
+- Room
+- DataStore Preferences
+- Dagger Hilt 
--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -3,4 +3,6 @@ package com.example.newsapp.util
 object Constants {
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+    const val API_KEY = "76b5ad807c3b4e4c986edd7bead7f9a3"
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
