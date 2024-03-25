41c1d5c75df38169b704411099467e38142df280
Ingrid Silva <sinngtec@gmail.com>
2024-03-25 06:39:23 -0300

    Add app module

--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -2,32 +2,71 @@ package com.example.newsapp.di
 
 import android.app.Application
 import com.example.newsapp.data.manger.LocalUserMangerImpl
+import com.example.newsapp.data.remote.NewsApi
+import com.example.newsapp.data.repoitory.NewsRepositoryImpl
 import com.example.newsapp.domain.manger.LocalUserManger
-import com.example.newsapp.domain.usecases.AppEntryUseCases
-import com.example.newsapp.domain.usecases.ReadAppEntry
-import com.example.newsapp.domain.usecases.SaveAppEntry
+import com.example.newsapp.domain.repository.NewsRepository
+import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
+import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
+import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.domain.usecases.news.GetNews
+import com.example.newsapp.domain.usecases.news.NewsUseCases
+import com.example.newsapp.domain.usecases.news.SearchNews
+import com.example.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
-
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Provides
     @Singleton
     fun provideLocalUserManger(
         application: Application
-    ): LocalUserManger = LocalUserMangerImpl(application)
-
+    ): LocalUserManger = LocalUserMangerImpl(context = application)
 
     @Provides
     @Singleton
     fun provideAppEntryUseCases(
         localUserManger: LocalUserManger
-    ) = AppEntryUseCases(
+    ): AppEntryUseCases = AppEntryUseCases(
         readAppEntry = ReadAppEntry(localUserManger),
         saveAppEntry = SaveAppEntry(localUserManger)
     )
+
+    @Provides
+    @Singleton
+    fun provideApiInstance(): NewsApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ): NewsRepository {
+        return NewsRepositoryImpl(newsApi)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository
+    ): NewsUseCases {
+        return NewsUseCases(
+            getNews = GetNews(newsRepository),
+            searchNews = SearchNews(newsRepository)
+        )
+    }
+
 }
