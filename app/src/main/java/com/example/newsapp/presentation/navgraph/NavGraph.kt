b1c2bb3a9c07e1d6e91e397877c19ec34bea8d3b
Ingrid Silva <sinngtec@gmail.com>
2024-03-25 06:33:51 -0300

    add nav graph

--- app/src/main/java/com/example/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/newsapp/presentation/navgraph/NavGraph.kt
@@ -1,4 +1,46 @@
 package com.example.newsapp.presentation.navgraph
 
-class NavGraph {
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import com.example.newsapp.onboarding.OnBoardingScreen
+import com.example.newsapp.onboarding.OnBoardingViewModel
+import com.example.newsapp.presentation.search.SearchScreen
+import com.example.newsapp.presentation.search.SearchViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = startDestination) {
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ) {
+            composable(
+                route = Route.OnBoardingScreen.route
+            ) {
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(event = viewModel::onEvent)
+            }
+        }
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.NewsNavigatorScreen.route
+        ) {
+            composable(
+                route = Route.NewsNavigatorScreen.route
+            ) {
+//                val viewModel: HomeViewModel = hiltViewModel()
+//                val articles = viewModel.news.collectAsLazyPagingItems()
+//                HomeScreen(articles = articles, navigate = {})
+                val viewModel: SearchViewModel = hiltViewModel()
+                SearchScreen(state = viewModel.state.value, event = viewModel::onEvent, navigate = {})
+            }
+        }
+    }
 }
