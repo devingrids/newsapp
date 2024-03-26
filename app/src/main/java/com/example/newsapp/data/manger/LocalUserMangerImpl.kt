6fb5c5369bf163ba9fe21b3757599c420e070faf
Ingrid Silva <sinngtec@gmail.com>
2024-03-26 00:11:14 -0300

    add news repository impl

--- app/src/main/java/com/example/newsapp/data/manger/LocalUserMangerImpl.kt
+++ app/src/main/java/com/example/newsapp/data/manger/LocalUserMangerImpl.kt
@@ -15,23 +15,24 @@ import kotlinx.coroutines.flow.map
 class LocalUserMangerImpl(
     private val context: Context
 ) : LocalUserManger {
+
     override suspend fun saveAppEntry() {
         context.dataStore.edit { settings ->
-            settings[PreferencesKeys.APP_ENTRY] = true
+            settings[PreferenceKeys.APP_ENTRY] = true
         }
     }
 
     override fun readAppEntry(): Flow<Boolean> {
         return context.dataStore.data.map { preferences ->
-            preferences[PreferencesKeys.APP_ENTRY] ?: false
+            preferences[PreferenceKeys.APP_ENTRY] ?: false
         }
     }
 }
 
 private val readOnlyProperty = preferencesDataStore(name = USER_SETTINGS)
 
-private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = USER_SETTINGS)
+val Context.dataStore: DataStore<Preferences> by readOnlyProperty
 
-private object PreferencesKeys {
-    val APP_ENTRY = booleanPreferencesKey(name = Constants.APP_ENTRY)
+private object PreferenceKeys {
+    val APP_ENTRY = booleanPreferencesKey(Constants.APP_ENTRY)
 }
--- app/src/main/java/com/example/newsapp/data/repoitory/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/newsapp/data/repoitory/NewsRepositoryImpl.kt
@@ -3,15 +3,18 @@ package com.example.newsapp.data.repoitory
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import com.example.newsapp.data.local.NewsDao
 import com.example.newsapp.data.remote.NewsApi
 import com.example.newsapp.data.remote.NewsPagingSource
 import com.example.newsapp.data.remote.SearchNewsPagingSource
 import com.example.newsapp.domain.model.Article
 import com.example.newsapp.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class NewsRepositoryImpl(
-    private val newsApi: NewsApi
+class NewsRepositoryImpl @Inject constructor(
+    private val newsApi: NewsApi,
+    private val newsDao: NewsDao
 ) : NewsRepository {
     override fun getNews(sources: List<String>): Flow<PagingData<Article>> {
         return Pager(
@@ -37,4 +40,21 @@ class NewsRepositoryImpl(
             }
         ).flow
     }
+
+    override suspend fun upsertArticle(article: Article) {
+        newsDao.upsert(article)
+    }
+
+    override suspend fun deleteArticle(article: Article) {
+        newsDao.delete(article)
+    }
+
+    override fun getArticles(): Flow<List<Article>> {
+        return newsDao.getArticles()
+    }
+
+    override suspend fun getArticle(url: String): Article? {
+        return newsDao.getArticle(url = url)
+    }
+
 }
